2c6d3fa243b6a9947bcb77d64ec3f558c7ff459c
Stanislav Romanyshyn <romanyshyn6696@gmail.com>
2024-06-12 11:40:28 +0300

    make build

---
+++ index.47aeb541.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery");e.addEventListener("click",function(e){e.preventDefault()}),e.addEventListener("click",function(e){var t=e.target.closest(".list-item").querySelector(".list-item__link"),r=document.getElementById("largeImg");t&&(r.src=t.href)},{capture:!0});
+//# sourceMappingURL=index.47aeb541.js.map
